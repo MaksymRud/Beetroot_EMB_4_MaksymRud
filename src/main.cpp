f6331b6f75fb057908d41ffc0bc3655e037be541
maksymrud <maksymrud607@gmail.com>
2026-03-17 00:10:52 +0200

    copy of hw22

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,62 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
 
-#define LED_PIN     48  // YD-ESP32-S3 onboard WS2812 (IO48)
-#define NUMPIXELS   1   // One RGB LED only
+constexpr uint8_t Relay_Control_Pin = 4;
+constexpr uint8_t Reley_Open_Pin = 38;
+constexpr uint8_t Button_Pin = 6; // ms
+volatile bool relayIsOpen = false;
+volatile bool buttonPressed = false;
+unsigned long control_input_time = 0;
+volatile unsigned long lastButtonInterruptTime = 0;
+volatile unsigned long lastRelayInterruptTime = 0;
+unsigned long relay_open_time = 0;
 
-Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
-
-void setup() {
-    Serial.begin(115200);
-    pixels.begin();
-    pixels.setBrightness(100); // Brightness (0~255)
-    Serial.println("Rainbow demo start with brightness 100");
-}
-
-uint32_t Wheel(byte pos) {
-    pos = 255 - pos;
-    if(pos < 85) {
-        return pixels.Color(255 - pos * 3, 0, pos * 3);
-    } else if(pos < 170) {
-        pos -= 85;
-        return pixels.Color(0, pos * 3, 255 - pos * 3);
-    } else {
-        pos -= 170;
-        return pixels.Color(pos * 3, 255 - pos * 3, 0);
+void IRAM_ATTR handleRelayOpenInterrupt() {
+    unsigned long currentTime = millis();
+    if (currentTime - lastRelayInterruptTime > 50) {
+        lastRelayInterruptTime = currentTime;
+        relayIsOpen = true;
     }
 }
 
+void IRAM_ATTR handleButtonInterrupt() {
+    unsigned long currentTime = millis();
+    if (currentTime - lastButtonInterruptTime > 50) {
+        lastButtonInterruptTime = currentTime;
+        buttonPressed = true;
+    }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(115200);
+    pinMode(Relay_Control_Pin, OUTPUT);
+    pinMode(Reley_Open_Pin, INPUT_PULLDOWN);
+    pinMode(Button_Pin, INPUT_PULLDOWN);
+    attachInterrupt(digitalPinToInterrupt(Button_Pin), handleButtonInterrupt, RISING);
+    attachInterrupt(digitalPinToInterrupt(Reley_Open_Pin), handleRelayOpenInterrupt, RISING);
+    digitalWrite(Relay_Control_Pin, LOW);
+}
+
 void loop() {
-    for(int i = 0; i < 256; i++) {
-        pixels.setPixelColor(0, Wheel(i));
-        pixels.show();
-        delay(20);
+    if (buttonPressed) {
+        if (digitalRead(Relay_Control_Pin) == LOW) {
+            digitalWrite(Relay_Control_Pin, HIGH);
+            control_input_time = millis();
+            Serial.printf("Try to open relay! Time: %lu ms\n", control_input_time);
+        } else {
+            digitalWrite(Relay_Control_Pin, LOW);
+            Serial.printf("Try to close relay!\n");
+        }
+        buttonPressed = false;
+    }
+
+    if (relayIsOpen) {
+        relayIsOpen = false;
+        relay_open_time = millis();
+        if (digitalRead(Relay_Control_Pin) == LOW) {
+            Serial.printf("Relay debounced on closing!\n");
+        } else {
+            Serial.printf("Relay opened! Time since control input: %lu ms\n", relay_open_time - control_input_time);
+        }
     }
 }
