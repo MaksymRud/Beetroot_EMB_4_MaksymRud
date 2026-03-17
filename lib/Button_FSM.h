8af870556129bb5fe591f78541cc2ae7f0ba0a05
maksymrud <maksymrud607@gmail.com>
2026-03-18 00:30:34 +0200

    add fsm button

---
+++ lib/Button_FSM.h
@@ -0,0 +1,26 @@
+#ifndef BUTTON_FSM_H
+#define BUTTON_FSM_H
+
+#include <Arduino.h>
+
+typedef enum {
+    STATE_IDLE,
+    STATE_PRESSED,
+    STATE_HELD,
+    STATE_RELEASED
+} ButtonState_t;
+
+typedef struct {
+    uint8_t pin;
+    ButtonState_t state;
+    uint16_t debounce_time;
+    uint16_t short_press_time;
+    uint16_t long_press_time;
+    uint32_t lastChangeTime;
+} Button_FSM_t;
+
+void Button_FSM_Init(Button_FSM_t *button, uint8_t pin, uint16_t debounce_time, uint16_t short_press_time, uint16_t long_press_time);
+void Button_FSM_Update(Button_FSM_t *button);
+bool Button_FSM_IsPressed(Button_FSM_t *button, bool &is_long_press);
+
+#endif // BUTTON_FSM_H
