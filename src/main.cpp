6129075adf9de9ce518aad06aefd6388951d55f8
maksymrud <maksymrud607@gmail.com>
2026-03-29 18:53:29 +0300

    refactoring of a state debaunce

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include <Arduino.h>
 #include <atomic>
 
+enum ButtonState {
+    ButtonPressed,
+    ButtonHeld,
+    ButtonReleased,
+    ButtonIdle
+};
+
 constexpr uint8_t Button_Pin = 6;
+constexpr uint8_t LogicAnalyzer_Pin = 21;
+constexpr unsigned long debounceDelay_us = 50000; // debounce delay in microseconds
 volatile bool buttonPressed = false;
 volatile unsigned long lastButtonInterruptTime = 0;
+unsigned long buttonAcceptedTime_us = 0;
+unsigned long buttonStartWaitAcceptanceTime_us = 0;
+unsigned long lastButtonStateChangeTime = 0;
 std::atomic<int> interrupts_counter(0);
 std::atomic<int> accepted_counter(0);
-hw_timer_t *Timer = nullptr;
-constexpr long WorkingTime = 30000000;
 volatile bool timerExpired = false;
+uint8_t buttonState = ButtonIdle;
 
 /*
     Task 3: state-based debounce using pin level
@@ -18,51 +29,65 @@ volatile bool timerExpired = false;
       HIGH (released) → ignore (bounce / release)
 */
 
-void ARDUINO_ISR_ATTR onStopFanTimer() {
-    timerExpired = true;
-}
-
 void IRAM_ATTR handleButtonInterrupt() {
     interrupts_counter.fetch_add(1);
-    lastButtonInterruptTime = millis();
+    lastButtonInterruptTime = micros();
     buttonPressed = true;
-}
-
-void startTimer() {
-    Timer = timerBegin(0, 80, true);
-    timerAttachInterrupt(Timer, &onStopFanTimer, true);
-    timerAlarmWrite(Timer, WorkingTime, true);
-    timerAlarmEnable(Timer);
-    Serial.println("Timer started...");
 }
 
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(115200);
     pinMode(Button_Pin, INPUT_PULLUP);
+    pinMode(LogicAnalyzer_Pin, OUTPUT);
+    digitalWrite(LogicAnalyzer_Pin, LOW);
     attachInterrupt(digitalPinToInterrupt(Button_Pin), handleButtonInterrupt, FALLING);
-    startTimer();
 }
 
 void loop() {
     if (buttonPressed) {
-        buttonPressed = false;
-
         int pinState = digitalRead(Button_Pin);
-
-        if (pinState == LOW) {                       // button is still held → real press
-            accepted_counter.fetch_add(1);
-            unsigned long dt = millis() - lastButtonInterruptTime;
-            Serial.printf("Button pressed (pin LOW)! Accepted: %d, Raw interrupts: %d, ISR->accept: %lu ms\n",
+        switch (buttonState)
+        {
+            case ButtonState::ButtonIdle:
+                if (pinState == LOW) {
+                    lastButtonStateChangeTime = micros();
+                    buttonStartWaitAcceptanceTime_us = micros();
+                    buttonState = ButtonState::ButtonPressed;
+                } else {
+                    buttonState = ButtonState::ButtonIdle;
+                }
+                break;
+            case ButtonState::ButtonPressed:
+                if (pinState == LOW) {
+                    if (micros() - lastButtonStateChangeTime >= debounceDelay_us) {
+                        buttonState = ButtonState::ButtonHeld;
+                    }
+                }
+                break;
+            case ButtonState::ButtonHeld:
+                if (pinState == LOW) {
+                    buttonState = ButtonState::ButtonHeld;
+                    accepted_counter.fetch_add(1);
+                    unsigned long dt = micros() - buttonStartWaitAcceptanceTime_us;
+                    Serial.printf("Button pressed (pin LOW)! Accepted: %d, Raw interrupts: %d, ISR->accept: %lu ms\n",
                           accepted_counter.load(), interrupts_counter.load(), dt);
-        } else {                                     // pin already HIGH → bounce / release
-            Serial.printf("Bounce ignored (pin HIGH). Raw interrupts: %d\n",
-                          interrupts_counter.load());
+                } else {
+                    lastButtonStateChangeTime = micros();
+                    buttonState = ButtonState::ButtonReleased;
+                }
+                break;
+            case ButtonState::ButtonReleased:
+                if (pinState == HIGH) {
+                    if (micros() - lastButtonStateChangeTime >= debounceDelay_us) {
+                        buttonState = ButtonState::ButtonIdle;
+                        buttonPressed = false;
+                    }
+                };
+                break;
+            default:
+                break;
         }
     }
-    if (timerExpired) {
-        Serial.printf("Timer expired! Accepted: %d, Raw interrupts: %d\n",
-                      accepted_counter.load(), interrupts_counter.load());
-        timerExpired = false;
-    }
+    digitalWrite(LogicAnalyzer_Pin, digitalRead(Button_Pin));
 }
