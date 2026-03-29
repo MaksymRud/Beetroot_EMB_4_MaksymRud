6b761b534eafc099bc688bbbadc6a369b43294c7
maksymrud <maksymrud607@gmail.com>
2026-03-29 17:12:28 +0300

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,13 @@
 #include <atomic>
 
 constexpr uint8_t Button_Pin = 6;
-constexpr unsigned long DebounceDelay = 50; // ms
+constexpr uint8_t LogicAnalyzer_Pin = 21;
+constexpr unsigned long DebounceDelay_us = 50000;
 volatile bool buttonPressed = false;
 volatile unsigned long lastButtonInterruptTime = 0;
 unsigned long lastAcceptedPressTime = 0;
 std::atomic<int> interrupts_counter(0);
 std::atomic<int> accepted_counter(0);
-hw_timer_t *Timer = nullptr;
-constexpr long WorkingTime = 30000000;
 volatile bool timerExpired = false;
 
 /*
@@ -18,49 +17,34 @@ volatile bool timerExpired = false;
     - Debounce check (< 50 ms) is done in loop()
 */
 
-void ARDUINO_ISR_ATTR onStopFanTimer() {
-    timerExpired = true;
-}
-
 void IRAM_ATTR handleButtonInterrupt() {
     interrupts_counter.fetch_add(1);
-    lastButtonInterruptTime = millis();
+    lastButtonInterruptTime = micros();
     buttonPressed = true;
-}
-
-void startTimer() {
-    Timer = timerBegin(0, 80, true);
-    timerAttachInterrupt(Timer, &onStopFanTimer, true);
-    timerAlarmWrite(Timer, WorkingTime, true);
-    timerAlarmEnable(Timer);
-    Serial.println("Timer started...");
 }
 
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(115200);
     pinMode(Button_Pin, INPUT_PULLUP);
+    pinMode(LogicAnalyzer_Pin, OUTPUT);
+    digitalWrite(LogicAnalyzer_Pin, LOW);
     attachInterrupt(digitalPinToInterrupt(Button_Pin), handleButtonInterrupt, FALLING);
-    startTimer();
 }
 
 void loop() {
     if (buttonPressed) {
         buttonPressed = false;
-        unsigned long now = millis();
-        if (now - lastAcceptedPressTime >= DebounceDelay) {
+        unsigned long now = micros();
+        if (now - lastAcceptedPressTime >= DebounceDelay_us) {
             lastAcceptedPressTime = now;
             accepted_counter.fetch_add(1);
-            Serial.printf("Button pressed! Accepted: %d, Raw interrupts: %d, Time since last: %lu ms\n",
+            Serial.printf("Button pressed! Accepted: %d, Raw interrupts: %d, Time since last: %lu us\n",
                           accepted_counter.load(), interrupts_counter.load(), now - lastButtonInterruptTime);
         } else {
-            Serial.printf("Bounce ignored (dt=%lu ms). Raw interrupts: %d\n",
+            Serial.printf("Bounce ignored (dt=%lu us). Raw interrupts: %d\n",
                           now - lastAcceptedPressTime, interrupts_counter.load());
         }
     }
-    if (timerExpired) {
-        Serial.printf("Timer expired! Accepted: %d, Raw interrupts: %d\n",
-                      accepted_counter.load(), interrupts_counter.load());
-        timerExpired = false;
-    }
+    digitalWrite(LogicAnalyzer_Pin, !digitalRead(LogicAnalyzer_Pin)); // Toggle logic analyzer pin for timing visualization
 }
